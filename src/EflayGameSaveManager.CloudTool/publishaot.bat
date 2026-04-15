a0cd0445c64eec5c84939b56f736b966e0fd7802
GXR <gxrsprite@hotmail.com>
2026-04-15 22:14:12 +0800

    fix cloudtool

--- src/EflayGameSaveManager.CloudTool/publishaot.bat
+++ src/EflayGameSaveManager.CloudTool/publishaot.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet publish .\EflayGameSaveManager.CloudTool.csproj -c Release -r win-x64 -f net10.0 --self-contained true /p:PublishAot=true /p:PublishTrimmed=true -o ..\EflayGameSaveManager.Lazarus\bin
+dotnet publish .\EflayGameSaveManager.CloudTool.csproj -c Release -r win-x64 -f net10.0 --self-contained true /p:PublishAot=true /p:PublishTrimmed=true /p:InvariantGlobalization=true -o ..\EflayGameSaveManager.Lazarus\bin
